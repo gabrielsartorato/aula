71120fcd3c8e6bdc8b05f5992a55f624b2b84ede
NagylaMota <42254697+NagylaMota@users.noreply.github.com>
2020-06-17 23:00:21 -0300

    ultima alteraçao

--- app_aula/lib/cadastro.dart
+++ app_aula/lib/cadastro.dart
@@ -1,10 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:app_aula/lista.dart';
 // import 'package:app_aula/cadastro.dart';
 
 // void main() => runApp(LoginApp());
-
 
 class Cadastro extends StatefulWidget {
   @override
@@ -14,7 +14,8 @@ class Cadastro extends StatefulWidget {
 }
 
 class _Cadastro extends State<Cadastro> {
-
+  String username = "";
+  String password = "";
   // f45d27
   // f5851f
 
@@ -32,32 +33,26 @@ class _Cadastro extends State<Cadastro> {
           children: <Widget>[
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height/2.5,
+              height: MediaQuery.of(context).size.height / 2.5,
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color(0xFF6495ED),
-                    Color(0xFF4169E1)
-                  ],
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(90)
-                )
-              ),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color(0xFF6495ED), Color(0xFF4169E1)],
+                  ),
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(90))),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Spacer(),
                   Align(
                     alignment: Alignment.center,
-                     child:
-                     Container(
-                      height: 150,
-                      width: 150, 
-                      child: 
-                     Image.asset('imagens/drfofo.png',alignment:Alignment.center)), 
+                    child: Container(
+                        height: 150,
+                        width: 150,
+                        child: Image.asset('imagens/drfofo.png',
+                            alignment: Alignment.center)),
                   ),
                   // Padding(
                   //   padding: const EdgeInsets.only(right: 10, top: 10),
@@ -72,95 +67,82 @@ class _Cadastro extends State<Cadastro> {
                 ],
               ),
             ),
-
             Expanded(
-                          child: Container(
-                height: MediaQuery.of(context).size.height/2,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2,
                 width: MediaQuery.of(context).size.width,
                 padding: EdgeInsets.only(top: 62),
                 child: Column(
                   children: <Widget>[
                     Container(
-                      width: MediaQuery.of(context).size.width/1.2,
+                      width: MediaQuery.of(context).size.width / 1.2,
                       height: 45,
                       padding: EdgeInsets.only(
-                        top: 4,left: 16, right: 16, bottom: 4
-                      ),
+                          top: 4, left: 16, right: 16, bottom: 4),
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(50)
-                        ),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 5
-                          )
-                        ]
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(color: Colors.black12, blurRadius: 5)
+                          ]),
                       child: TextField(
+                        // nome de usuario
+                        onChanged: (text) {
+                          username = text;
+                        },
                         decoration: InputDecoration(
                           border: InputBorder.none,
-                          icon: Icon(Icons.account_circle,
-                              color: Colors.grey,
+                          icon: Icon(
+                            Icons.account_circle,
+                            color: Colors.grey,
                           ),
-                            hintText: 'Escolha um nome de usuario',
+                          hintText: 'Escolha um nome de usuario',
                         ),
                       ),
                     ),
                     Container(
-                      width: MediaQuery.of(context).size.width/1.2,
+                      width: MediaQuery.of(context).size.width / 1.2,
                       height: 45,
                       margin: EdgeInsets.only(top: 32),
                       padding: EdgeInsets.only(
-                          top: 4,left: 16, right: 16, bottom: 4
-                      ),
+                          top: 4, left: 16, right: 16, bottom: 4),
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(50)
-                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
                           color: Colors.white,
                           boxShadow: [
-                            BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 5
-                            )
-                          ]
-                      ),
+                            BoxShadow(color: Colors.black12, blurRadius: 5)
+                          ]),
                       child: TextField(
+                        onChanged: (text) {
+                          password = text;
+                        },
                         decoration: InputDecoration(
                           border: InputBorder.none,
-                          icon: Icon(Icons.vpn_key,
+                          icon: Icon(
+                            Icons.vpn_key,
                             color: Colors.grey,
                           ),
                           hintText: 'Digite a sua senha',
                         ),
                       ),
                     ),
-
                     Container(
-                      width: MediaQuery.of(context).size.width/1.2,
+                      width: MediaQuery.of(context).size.width / 1.2,
                       height: 45,
                       margin: EdgeInsets.only(top: 32),
                       padding: EdgeInsets.only(
-                          top: 4,left: 16, right: 16, bottom: 4
-                      ),
+                          top: 4, left: 16, right: 16, bottom: 4),
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(50)
-                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
                           color: Colors.white,
                           boxShadow: [
-                            BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 5
-                            )
-                          ]
-                      ),
+                            BoxShadow(color: Colors.black12, blurRadius: 5)
+                          ]),
                       child: TextField(
                         decoration: InputDecoration(
                           border: InputBorder.none,
-                          icon: Icon(Icons.vpn_key,
+                          icon: Icon(
+                            Icons.vpn_key,
                             color: Colors.grey,
                           ),
                           hintText: 'Confirme a sua senha',
@@ -171,27 +153,32 @@ class _Cadastro extends State<Cadastro> {
                     Padding(
                       padding: const EdgeInsets.only(bottom: 40),
                       child: Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.fromLTRB(21,28,21,0),
-                      child: MaterialButton(
-                      minWidth: double.infinity,
-                      height: 48,
-                      child: Text('Cadastrar',
-                      style: TextStyle(
-                        fontSize: 14, color: Colors.white, fontFamily: "Roboto" ),
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.fromLTRB(21, 28, 21, 0),
+                        child: MaterialButton(
+                          minWidth: double.infinity,
+                          height: 48,
+                          child: Text(
+                            'Cadastrar',
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                fontFamily: "Roboto"),
+                          ),
+                          color: HexColor('#4169E1'),
+                          //!ON CLICK DO BOTÃO
+                          onPressed: () async {
+                            print(username);
+                            print(password);
+                            FirebaseUser user = await FirebaseAuth.instance
+                                .createUserWithEmailAndPassword(
+                                    email: username, password: password);
+                          },
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(40.0),
+                          ),
                         ),
-                      color: HexColor('#4169E1'),
-                      //!ON CLICK DO BOTÃO
-                      onPressed:(){
-                      Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Listahospitais()),
-                                );
-                      },
-                      shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(40.0), ),
-                  ),
-                ),
+                      ),
                     ),
                   ],
                 ),
@@ -203,7 +190,6 @@ class _Cadastro extends State<Cadastro> {
     );
   }
 }
-
 
 class HexColor extends Color {
   static int _getColorFromHex(String hexColor) {
